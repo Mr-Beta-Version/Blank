707015dd1c263918d93fb616c9a748744387603c
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-02-02 11:49:11 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -7,6 +7,7 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import ownidea
+    from ownidea import menu
+    menu()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
